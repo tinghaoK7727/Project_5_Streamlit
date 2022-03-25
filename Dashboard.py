0b3cde70533da4f529ddb84a3834680293da5e4d
Vito <vito.coquet@gmail.com>
2022-03-25 12:15:46 +0100

    changing stuff

--- Dashboard.py
+++ Dashboard.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 with st.container():
     st.markdown('# Heart')
-    st.file_uploader(label='upload .csv', type='csv')
-    # df = pd.read_csv('heart.csv')
-    # st.dataframe(df)
+    # st.file_uploader(label='upload .csv', type='csv')
+    df = pd.read_csv('heart.csv')
+    st.dataframe(df)
 

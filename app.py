2f1c3cd7dd0cc59cf6fb366d92fd2085835487e9
Vito <vito.coquet@gmail.com>
2022-03-25 15:59:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ import matplotlib.pyplot as plt
 #     sns.countplot(x = "year", data = df)
 #     st.pyplot(fig)
 
-# st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 with st.container():
     st.markdown('# Heart Conditions Analysis ')
     df = pd.read_csv('heart.csv')
+    col1, col2 = st.columns(2)
     st.dataframe(df)
     st.bar_chart(df)
     # sns.lineplot(df)
@@ -47,97 +48,100 @@ with st.container():
     # df2['target'] = df2['target'].apply(chng2)
 
 
+    with col1:
+
+        sns.countplot(data= df2, x='sex',hue='target')
+        plt.title('Gender v/s target\n')
+        
+        st.pyplot()
 
 
-    sns.countplot(data= df2, x='sex',hue='target')
-    plt.title('Gender v/s target\n')
-    
-    st.pyplot()
+        sns.countplot(data= df2, x='cp',hue='target')
+        plt.title('Chest Pain Type v/s target\n')
+        st.pyplot()
 
 
-    sns.countplot(data= df2, x='cp',hue='target')
-    plt.title('Chest Pain Type v/s target\n')
-    st.pyplot()
+        sns.countplot(data= df2, x='sex',hue='thal')
+        plt.title('Gender v/s Thalassemia\n')
+        print('Thalassemia (thal-uh-SEE-me-uh) is an inherited blood disorder that causes your body to have less hemoglobin than normal. Hemoglobin enables red blood cells to carry oxygen')
+        st.pyplot()
+
+        sns.countplot(data= df2, x='slope',hue='target')
+        plt.title('Slope v/s Target\n') 
+        st.pyplot()
+
+        sns.countplot(data= df2, x='exang',hue='thal')
+        plt.title('exang v/s Thalassemia\n')
+        st.pyplot()
+
+        plt.figure(figsize=(16,7))
+        sns.distplot(df[df['target']==0]['age'],kde=False,bins=50)
+        plt.title('Age of Heart Diseased Patients\n')
+        st.pyplot()
+
+        plt.figure(figsize=(16,7))
+        sns.distplot(df[df['target']==0]['chol'],kde=False,bins=40)
+        plt.title('Chol of Heart Diseased Patients\n')
+        st.pyplot()
+
+        plt.figure(figsize=(16,7))
+        sns.distplot(df[df['target']==0]['thalach'],kde=False,bins=40)
+        plt.title('thalach of Heart Diseased Patients\n')
+        st.pyplot()
+
+        df3 = df[df['target'] == 0 ][['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach',
+        'exang', 'oldpeak', 'slope', 'ca', 'thal', 'target']] 
 
 
-    sns.countplot(data= df2, x='sex',hue='thal')
-    plt.title('Gender v/s Thalassemia\n')
-    print('Thalassemia (thal-uh-SEE-me-uh) is an inherited blood disorder that causes your body to have less hemoglobin than normal. Hemoglobin enables red blood cells to carry oxygen')
-    st.pyplot()
 
-    sns.countplot(data= df2, x='slope',hue='target')
-    plt.title('Slope v/s Target\n') 
-    st.pyplot()
+    with col2:
+        pal = sns.light_palette("blue", as_cmap=True)
+        print('Age vs trestbps(Heart Diseased Patinets)')
+        sns.jointplot(data=df3, x='age', y='trestbps', kind='hex', cmap='Reds')
+        st.pyplot()
 
-    sns.countplot(data= df2, x='exang',hue='thal')
-    plt.title('exang v/s Thalassemia\n')
-    st.pyplot()
-
-    plt.figure(figsize=(16,7))
-    sns.distplot(df[df['target']==0]['age'],kde=False,bins=50)
-    plt.title('Age of Heart Diseased Patients\n')
-    st.pyplot()
-
-    plt.figure(figsize=(16,7))
-    sns.distplot(df[df['target']==0]['chol'],kde=False,bins=40)
-    plt.title('Chol of Heart Diseased Patients\n')
-    st.pyplot()
-
-    plt.figure(figsize=(16,7))
-    sns.distplot(df[df['target']==0]['thalach'],kde=False,bins=40)
-    plt.title('thalach of Heart Diseased Patients\n')
-    st.pyplot()
-
-    df3 = df[df['target'] == 0 ][['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach',
-       'exang', 'oldpeak', 'slope', 'ca', 'thal', 'target']] 
-
-    pal = sns.light_palette("blue", as_cmap=True)
-    print('Age vs trestbps(Heart Diseased Patinets)')
-    sns.jointplot(data=df3, x='age', y='trestbps', kind='hex', cmap='Reds')
-    st.pyplot()
-
-    sns.jointplot(data=df3, x='chol', y='age', kind='kde', cmap='PuBu')
-    st.pyplot()
+        sns.jointplot(data=df3, x='chol', y='age', kind='kde', cmap='PuBu')
+        st.pyplot()
 
 
-    sns.jointplot(data=df3, x='chol', y='trestbps', kind='resid')
-    st.pyplot()
+        sns.jointplot(data=df3, x='chol', y='trestbps', kind='resid')
+        st.pyplot()
 
-    sns.boxplot(data=df2,x='target',y='age')
-    st.pyplot()
+        sns.boxplot(data=df2,x='target',y='age')
+        st.pyplot()
 
-    plt.figure(figsize=(14,8))
-    sns.violinplot(data=df2,x='ca',y='age',hue='target')
-    st.pyplot()
+        plt.figure(figsize=(14,8))
+        sns.violinplot(data=df2,x='ca',y='age',hue='target')
+        st.pyplot()
 
-    sns.boxplot(data=df2,x='cp',y='thalach',hue='target')
-    st.pyplot()
+        sns.boxplot(data=df2,x='cp',y='thalach',hue='target')
+        st.pyplot()
 
-    plt.figure(figsize=(10,7))
-    sns.boxplot(data=df2,x='fbs',y='trestbps',hue='target')
-    st.pyplot()
+        plt.figure(figsize=(10,7))
+        sns.boxplot(data=df2,x='fbs',y='trestbps',hue='target')
+        st.pyplot()
 
-    plt.figure(figsize=(10,7))
-    sns.violinplot(data=df2,x='exang',y='oldpeak',hue='target')
-    st.pyplot()
+        plt.figure(figsize=(10,7))
+        sns.violinplot(data=df2,x='exang',y='oldpeak',hue='target')
+        st.pyplot()
 
-    plt.figure(figsize=(10,7))
-    sns.boxplot(data=df2,x='slope',y='thalach',hue='target')
-    st.pyplot()
+        plt.figure(figsize=(10,7))
+        sns.boxplot(data=df2,x='slope',y='thalach',hue='target')
+        st.pyplot()
 
-    sns.violinplot(data=df2,x='thal',y='oldpeak',hue='target')
-    st.pyplot()
+        sns.violinplot(data=df2,x='thal',y='oldpeak',hue='target')
+        st.pyplot()
 
-    sns.violinplot(data=df2,x='target',y='thalach')
-    st.pyplot()
+        sns.violinplot(data=df2,x='target',y='thalach')
+        st.pyplot()
 
-    sns.clustermap(df.corr(),annot=True)
-    st.pyplot()
+        sns.clustermap(df.corr(),annot=True)
+        st.pyplot()
 
-    sns.pairplot(df,hue='cp')
-    st.pyplot()
+        sns.pairplot(df,hue='cp')
+        st.pyplot()
 
-    
-# fig, [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10] = plt.subplots()
-# ax1.scatter([1, 2, 3], [1, 2, 3])
-# st.pyplot(fig)
+        
+    # fig, [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10] = plt.subplots()
+    # ax1.scatter([1, 2, 3], [1, 2, 3])
+    # st.pyplot(fig)

7e72affdd37308c38f6506dd04e25858a2f6ca33
Vito <vito.coquet@gmail.com>
2022-03-25 13:01:54 +0100

    trying

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+def countPlot():
+    fig = plt.figure(figsize=(10, 4))
+    sns.countplot(x = "year", data = df)
+    st.pyplot(fig)
+
 with st.container():
     st.markdown('# Heart Conditions Analysis ')
     df = pd.read_csv('heart.csv')
     st.dataframe(df)
     st.bar_chart(df)
     sns.lineplot(df)
+    countPlot()
 
 
     

6f18542ce7d401d2dda76905655ac16f1350aa64
Vito <vito.coquet@gmail.com>
2022-03-25 12:53:21 +0100

    trying to commit from vscode

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
 with st.container():
-    st.markdown('# Heart')
-    # st.file_uploader(label='upload .csv', type='csv')
+    st.markdown('# Heart Conditions Analysis ')
     df = pd.read_csv('heart.csv')
     st.dataframe(df)
     st.bar_chart(df)
+
+
+    

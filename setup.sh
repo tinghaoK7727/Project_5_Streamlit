67b93bea16c0c55bc0a145e41a240f95fb3ce3f5
Vito <vito.coquet@gmail.com>
2022-03-28 12:51:02 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
 echo "\
 [theme]\n\
-primaryColor = '#4ceaf5'\n\
-backgroundColor = '#EFEDE8'\n\
-secondaryBackgroundColor = '#fafafa'\n\
+primaryColor = '#3285a8'\n\
+backgroundColor = '#fafafa'\n\
+secondaryBackgroundColor = '#EFEDE8'\n\
 textColor= '#424242'\n\
 font = 'sans serif'\n\
 [server]\n\
